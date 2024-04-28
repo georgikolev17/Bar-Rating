c586b797ae3bfa51e1fec3111617457ec39a5cde
martin <martin@chtilianov.com>
2024-04-28 11:33:08 +0300

    forced_commit

--- Bar Rating/Migrations/ApplicationDbContextModelSnapshot.cs
+++ Bar Rating/Migrations/ApplicationDbContextModelSnapshot.cs
@@ -8,7 +8,7 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
 
-namespace Bar_Rating.Data.Migrations
+namespace Bar_Rating.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
     partial class ApplicationDbContextModelSnapshot : ModelSnapshot
